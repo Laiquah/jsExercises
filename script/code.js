34948a3aaf4b8529df4b201a200a7a4080b77341
Laiquah <cotielaiquah@gmail.com>
2023-05-24 15:00:16 +0200

    added changes to javascript

---
+++ script/code.js
@@ -0,0 +1,29 @@
+// variable - a variable is a name of storage locatiion. It could also be defined as a quantity that may be changed accroing to the mathematical problem.
+
+// value - Values in JavaScript include things like Booleans, Strings, arrays, integers, etc. Because values in JavaScript have specific properties that are employed in accordance with requirements and the run-time environment, its behavior is similar to that of other programming languages.
+
+// data type - Data types explain the many forms of data that we will be working with and storing in variables.
+
+// Initialization - Initialization means that memory is allocated for the variable by the JavaScript engine.
+
+// difference between let, var and const is:
+// If you never want a variable to change then you use const because it allows for no change of a variable.
+// If you want to reassign values and you want hoisting behaviour then var is the keyword to use.
+// If you dont want hoisting behaviour but you want to be able to change the variable then you use the keyword let.
+
+// Variable scope - This refers to the current context of code, which determines the accessibility of variables of Javascript.
+
+// you should use a for loop when the number of iterations is known and in case of no condition, the loop will be repeated infinite times.
+// you should use a while loop when the number of iterations is unknown and in the case of no condition, an error will be shown.
+// you only use a for each loop when you want to loop through all the values in an array or list.
+// this loop is the same as the while loop but you use it when you have a single forced loop at the start.
+
+// hoisting - The process by which the interpreter seems to relocate the definition of functions, variables, or classes to the top of their scope prior to code execution is referred to as hoisting.
+
+// variable scoping is defined in line 14, thank you.
+
+function fullName(){
+    console.log('Laiquah Cotie')
+}
+
+fullName()
